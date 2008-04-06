136a8418bbf0a31f4707ba1647d6f5312d7f988d
johnw@newartisans.com <johnw@newartisans.com>
2008-04-06 13:29:09 -0400

    changes

--- backup/large.ex
+++ backup/large.ex
@@ -7,11 +7,8 @@
 - /Archives/Windows/Neverwinter Nights/
 - /Archives/Windows/Half-Life 2/
 - /Archives/Windows/Descent 1 & 2/
+- /Archives/Windows/Descent 3/
 - /Archives/Windows/OED 3.1/
-- /Archives/Backups/Backup.dmg
-
-- /Documents/Pictures.ivc
-- /Documents/Scrapbook.dtBase/
 
 - /Library/Application Support/Coriolis/CDMaker/template.dmg
 
