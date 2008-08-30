03b86031f7118fbf05a111cc66657ae241d757bb
John Wiegley <johnw@newartisans.com>
2008-08-30 05:03:19 -0400

    Moved the firewall/ directory to its own project on GitHub.

--- firewall/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#sudo cp -p Firewall.hermes /Library/StartupItems/Firewall/Firewall
-sudo cp -p Firewall.hermes.sgu /Library/StartupItems/Firewall/Firewall
-sudo chown root:wheel /Library/StartupItems/Firewall/Firewall
-sudo chmod 755 /Library/StartupItems/Firewall/Firewall
-
-sudo /Library/StartupItems/Firewall/Firewall start
