414e51b2de34431bc0ef9fc4d0e3f0568f820419
John Wiegley <johnw@newartisans.com>
2011-10-04 03:27:24 -0500

    Changes

--- emacs-bootstrap.sh
+++ emacs-bootstrap.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-#make uninstall
-test -f Makefile && make distclean
-#rm -fr /Application/Misc/Emacs.app
-#rm -fr /usr/local/stow/emacs-release
+DEVEL=$HOME/.emacs.d/devel
 
-./configure --with-${1:-ns} CFLAGS=-O3 \
-    --prefix=/usr/local/stow/$(basename "$(pwd)") \
-    CPPFLAGS=-I/opt/local/include LDFLAGS=-L/opt/local/lib
+cd $DEVEL/build
 
-nice -n 20 make -j12 bootstrap
-#make install
+make clean
+
+../configure \
+    --with-${1:-ns} \
+    --prefix=$DEVEL/installed \
+    --enable-asserts \
+    CFLAGS='-g2 -ggdb' \
+    CPPFLAGS=-I/opt/local/include \
+    LDFLAGS=-L/opt/local/lib
+
+nice -n 20 make bootstrap
+#nice -n 20 make install
