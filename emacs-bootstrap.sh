9009c3521138a9276d25229fc511e037db38b299
John Wiegley <johnw@newartisans.com>
2011-12-12 03:06:26 -0600

    changes

--- emacs-bootstrap.sh
+++ emacs-bootstrap.sh
@@ -4,17 +4,18 @@ set -e
 
 DEVEL=$HOME/.emacs.d/devel
 
-cd $DEVEL/build
+cd $DEVEL
+git clean -dfx
+./autogen.sh
 
-make clean
+mkdir build
+mkdir target
+cd build
 
-../configure \
-    --with-${1:-ns} \
-    --prefix=$DEVEL/installed \
-    --enable-asserts \
+../configure --prefix=$DEVEL/target \
+    --with-ns --enable-asserts \
     CFLAGS='-g2 -ggdb' \
     CPPFLAGS=-I/opt/local/include \
     LDFLAGS=-L/opt/local/lib
 
-nice -n 20 make bootstrap
-#nice -n 20 make install
+nice -n 20 make "$@" bootstrap install
--- gcc-rebuild.sh
+++ gcc-rebuild.sh
@@ -1,12 +1,61 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e
 
-SRC=/usr/local/src
-STOW=/usr/local/stow
-GMP=$STOW/gmp-4.3.2
-MPFR=$STOW/mpfr-2.4.2
-MPC=$STOW/mpc-0.8.1
+if [[ ! -d gcc ]]; then
+    echo There is no gcc directory beneath the current directory
+    exit 1
+fi
+
+SRC=$(pwd)
+ROOT=$(dirname "$SRC")
+GMP=gmp-4.3.2
+MPFR=mpfr-2.4.2
+MPC=mpc-0.8.1
+major=4.7
+minor=0
+nodots=$(echo $major | sed 's/\.//')
+gccname=gcc-${major}
+gccsuffix=-mp-${major}
+boostmajor=1_48
+boost=${boostmajor}_0
+boostname=boost_$boost-gcc$nodots
+boostregex=libboost_regex-xgcc${nodots}-d-${boostmajor}.a
+
+if [[ "$1" == --prefix ]]; then
+    shift 1
+    if [[ "$1" == stow ]]; then
+        usestow=true
+        DEST="$ROOT/stow"
+        BIN="$ROOT/bin"
+        GMPDEST="$DEST/$GMP"
+        MPFRDEST="$DEST/$MPFR"
+        MPCDEST="$DEST/$MPC"
+        GCCDEST="$DEST/$gccname"
+        BOOSTDEST="$DEST/$boostname"
+    else
+        usestow=false
+        DEST="$1"
+        BIN="$1/bin"
+        GMPDEST="$DEST"
+        MPFRDEST="$DEST"
+        MPCDEST="$DEST"
+        GCCDEST="$DEST"
+        BOOSTDEST="$DEST"
+    fi
+    shift 1
+else
+    usestow=false
+    DEST="$ROOT/cpp11"
+    BIN="$ROOT/cpp11/bin"
+    GMPDEST="$DEST"
+    MPFRDEST="$DEST"
+    MPCDEST="$DEST"
+    GCCDEST="$DEST"
+    BOOSTDEST="$DEST"
+
+    test -d "$DEST" || mkdir "$DEST"
+fi
 
 if [[ -d /usr/src/redhat/BUILD ]]; then
     PRODUCTS=/usr/src/redhat/BUILD
@@ -16,13 +65,8 @@ else
     PRODUCTS=/tmp
 fi
 
-major=4.7
-minor=0
-gccname=gcc-${major}
-gccsuffix=-mp-${major}
-
-export PATH=/usr/bin:/bin
-export LD_LIBRARY_PATH=$GMP/lib:$MPFR/lib:$MPC/lib
+export PATH=/usr/bin:/bin:"$BIN"
+export LD_LIBRARY_PATH="$GMPDEST/lib:$MPFRDEST/lib:$MPCDEST/lib"
 
 if [[ -x /usr/bin/otool ]]; then
     export AR_FOR_TARGET=/usr/bin/ar
@@ -35,126 +79,153 @@ if [[ -x /usr/bin/otool ]]; then
     export OTOOL=/usr/bin/otool
     export OTOOL64=/usr/bin/otool
     export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
+    export OPTIONS=""
+    export GCCOPTIONS="--with-libiconv-prefix=/usr --with-system-zlib"
+else
+    export OPTIONS="--enable-static --disable-shared"
+    export GCCOPTIONS=""
 fi
+
+function cd_builddir()
+{
+    if [[ -d "$PRODUCTS" ]]; then
+        if [[ -d "$PRODUCTS/$1" ]]; then
+            rm -fr "$PRODUCTS/$1"
+        fi
+        mkdir -p "$PRODUCTS/$1"
+        cd "$PRODUCTS/$1"
+    fi
+}
 
 ########################################################################
 
-if [[ ! -d $GMP ]]; then
-    cd $SRC/$(basename $GMP)
-
-    if [[ -d "$PRODUCTS" ]]; then
-        if [[ -d "$PRODUCTS/$(basename $GMP)" ]]; then
-            rm -fr "$PRODUCTS/$(basename $GMP)"
-        fi
-        mkdir -p "$PRODUCTS/$(basename $GMP)"
-        cd "$PRODUCTS/$(basename $GMP)"
-    fi
+if [[ ! -f $GMPDEST/lib/libgmp.a ]]; then
+    cd $SRC/$GMP
+    cd_builddir $GMP
 
     test -f Makefile && make distclean
-    $SRC/$(basename $GMP)/configure --prefix=$GMP   \
-        2>&1 | tee gcc-rebuild.log
+    $SRC/$GMP/configure --prefix=$GMPDEST $OPTIONS      \
+        2>&1 | tee build.log
 
-    make "$@" 2>&1 | tee gcc-rebuild.log
+    make "$@" 2>&1 | tee build.log
     make install
+
+    if [[ $usestow == true ]]; then
+        cd $STOW
+        which stow && stow $GMP
+    fi
 fi
 
 ########################################################################
 
-if [[ ! -d $MPFR ]]; then
-    cd $SRC/$(basename $MPFR)
-
-    if [[ -d "$PRODUCTS" ]]; then
-        if [[ -d "$PRODUCTS/$(basename $MPFR)" ]]; then
-            rm -fr "$PRODUCTS/$(basename $MPFR)"
-        fi
-        mkdir -p "$PRODUCTS/$(basename $MPFR)"
-        cd "$PRODUCTS/$(basename $MPFR)"
-    fi
+if [[ ! -f $MPFRDEST/lib/libmpfr.a ]]; then
+    cd $SRC/$MPFR
+    cd_builddir $MPFR
 
     test -f Makefile && make distclean
-    $SRC/$(basename $MPFR)/configure --prefix=$MPFR \
-        --with-gmp=$GMP                             \
-        2>&1 | tee gcc-rebuild.log
+    $SRC/$MPFR/configure --prefix=$MPFRDEST $OPTIONS    \
+        --with-gmp=$GMPDEST                             \
+        2>&1 | tee build.log
 
-    make "$@" 2>&1 | tee gcc-rebuild.log
+    make "$@" 2>&1 | tee build.log
     make install
+
+    if [[ $usestow == true ]]; then
+        cd $STOW
+        which stow && stow $MPFR
+    fi
 fi
 
 ########################################################################
 
-if [[ ! -d $MPC ]]; then
-    cd $SRC/$(basename $MPC)
-
-    if [[ -d "$PRODUCTS" ]]; then
-        if [[ -d "$PRODUCTS/$(basename $MPC)" ]]; then
-            rm -fr "$PRODUCTS/$(basename $MPC)"
-        fi
-        mkdir -p "$PRODUCTS/$(basename $MPC)"
-        cd "$PRODUCTS/$(basename $MPC)"
-    fi
+if [[ ! -f $MPCDEST/lib/libmpc.a ]]; then
+    cd $SRC/$MPC
+    cd_builddir $MPC
 
     test -f Makefile && make distclean
-    $SRC/$(basename $MPC)/configure --prefix=$MPC   \
-        --with-gmp=$GMP --with-mpfr=$MPFR           \
-        2>&1 | tee gcc-rebuild.log
+    $SRC/$MPC/configure --prefix=$MPCDEST $OPTIONS      \
+        --with-gmp=$GMPDEST --with-mpfr=$MPFRDEST       \
+        2>&1 | tee build.log
 
-    make "$@" 2>&1 | tee gcc-rebuild.log
+    make "$@" 2>&1 | tee build.log
     make install
-fi
 
-########################################################################
-
-cd $STOW
-test -d ${gccname} && which stow && stow -D $gccname
-test -d ${gccname}.old && rm -fr ${gccname}.old
-test -d ${gccname} && mv $gccname ${gccname}.old
-
-cd $SRC/gcc
-
-if [[ -d "$PRODUCTS" ]]; then
-    if [[ -d "$PRODUCTS/$gccname" ]]; then
-        rm -fr "$PRODUCTS/$gccname"
+    if [[ $usestow == true ]]; then
+        cd $STOW
+        which stow && stow $MPC
     fi
-    mkdir -p "$PRODUCTS/$gccname"
-    cd "$PRODUCTS/$gccname"
 fi
-
-test -f Makefile && make distclean
-$SRC/gcc/configure --prefix=$STOW/$gccname      \
-    --disable-bootstrap                         \
-    --disable-multilib                          \
-    --disable-nls                               \
-    --enable-threads                            \
-    --enable-languages=c,c++                    \
-    --enable-stage1-checking                    \
-    --program-suffix=$gccsuffix                 \
-    --with-libiconv-prefix=/usr                 \
-    --with-gmp=$GMP                             \
-    --with-mpc=$MPC                             \
-    --with-mpfr=$MPFR                           \
-    --with-system-zlib                          \
-    2>&1 | tee gcc-rebuild.log
-
-make "$@" 2>&1 | tee gcc-rebuild.log
-make install
-
-cd $STOW
-rm -f /usr/local/stow/gcc-4.7/share/info/dir
-which stow && stow $gccname
 
 ########################################################################
 
-if [[ -d $HOME/src/ledger/lib && -d /Volumes/Boost ]]; then
-    cd $HOME/src/ledger/lib
-    make GCC_VERSION=$major clean
-    make GCC_VERSION=$major OPTJ="$@"
+if [[ ! -x $GCCDEST/bin/g++$gccsuffix ]]; then
+    if [[ $usestow == true ]]; then
+        cd $STOW
+        test -d ${gccname} && which stow && stow -D $gccname
+        test -d ${gccname}.old && rm -fr ${gccname}.old
+        test -d ${gccname} && mv $gccname ${gccname}.old
+    fi
 
-elif [[ -d /usr/local/src/boost_1_48_0 ]]; then
-    echo "using gcc : 4.7 : g++-mp-4.7 ;" > $HOME/user-config.jam
-    sh bootstrap.sh
-    ./b2 "$@" debug --prefix=$STOW/boost_1_48_0-gcc47           \
-        --build-dir=$PRODUCTS/boost_1_48_0-gcc47                \
-        --layout=versioned link=shared threading=single install
+    cd $SRC/gcc
+    cd_builddir $gccname
+
+    test -f Makefile && make distclean
+    $SRC/gcc/configure --prefix=$GCCDEST $GCCOPTIONS \
+        --disable-bootstrap                         \
+        --disable-multilib                          \
+        --disable-nls                               \
+        --enable-threads                            \
+        --enable-languages=c,c++                    \
+        --program-suffix=$gccsuffix                 \
+        --with-gmp=$GMPDEST                         \
+        --with-mpc=$MPCDEST                         \
+        --with-mpfr=$MPFRDEST                       \
+        2>&1 | tee build.log
+
+    make "$@" 2>&1 | tee build.log
+    make install
+
+    if [[ $usestow == true ]]; then
+        cd $STOW
+        rm -f $gccname/share/info/dir
+        which stow && stow $gccname
+    fi
+fi
+
+########################################################################
+
+if [[ ! -f $BOOSTDEST/lib/$boostregex ]]; then
+    if [[ -d $SRC/boost_$boost ]]; then
+        echo "using gcc : $major : g++$gccsuffix ;" > $HOME/user-config.jam
+
+        cd $SRC/boost_$boost
+        if [[ -x /usr/bin/otool ]]; then
+            perl -i -pe "s/local command = \\[ common\\.get-invocation-command darwin : g\\+\\+ : .*/local command = [ common.get-invocation-command darwin : g++ : g++$gccsuffix ] ;/;" tools/build/v2/tools/darwin.jam
+            perl -i -pe 's/flags darwin\.compile OPTIONS : -no-cpp-precomp -gdwarf-2 (-fexceptions )?;/flags darwin\.compile OPTIONS : -gdwarf-2 \1;/;' tools/build/v2/tools/darwin.jam
+        fi
+
+        cd_builddir $boostname
+        builddir=$(pwd)
+
+        cd $SRC/boost_$boost
+        sh bootstrap.sh
+        #./b2 "$@" --prefix=$BOOSTDEST --build-dir=$builddir --layout=versioned      \
+        #    --build-type=complete link=static define=_GLIBCXX__PTHREADS=1 install
+        ./b2 "$@" --prefix=$BOOSTDEST --build-dir=$builddir --layout=versioned      \
+            link=shared threading=single define=_GLIBCXX__PTHREADS=1 debug install
+
+        #cd status
+        #../b2 define=_GLIBCXX__PTHREADS=1
+
+        rm -f $HOME/user-config.jam
+
+    elif [[ -d $HOME/src/ledger/lib && -d /Volumes/Boost ]]; then
+        cd $HOME/src/ledger/lib
+
+        make GCC_VERSION=$major clean
+        make GCC_VERSION=$major OPTJ="$@"
+
+    fi
 fi
 
 ########################################################################
