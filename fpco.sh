7bdf1f0d32ab1578dafa4569f3c701ea059e4e1a
John Wiegley <johnw@newartisans.com>
2015-02-14 08:56:02 -0600

    git-annex automatic sync

--- fpco.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-cd ~/src/fpco
-
-./dev-scripts/build-all.sh --dev -j             \
-    --builddir $HOME/Products/fpco/dist         \
-    -s $HOME/Products/fpco/cabal-dev
-
-exit 0
