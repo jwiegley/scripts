07dae3ec4a2e2a2d39b6456a5f260d320383c55f
John Wiegley <johnw@newartisans.com>
2015-11-28 15:02:14 -0800

    git-annex in bin

---
+++ start-gpg-agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if test -f $HOME/.gpg-agent-info && \
+  kill -0 `cut -d: -f 2 $HOME/.gpg-agent-info` 2>/dev/null; then
+    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info`
+    export GPG_AGENT_INFO
+else
+  eval `/usr/local/MacGPG2/bin/gpg-agent --daemon --write-env-file`
+fi
+export GPG_TTY=`tty`
