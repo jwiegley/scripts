ce18e1e58f2f735785091eadf265397e0141b5d7
John Wiegley <john@dfinity.org>
2019-11-14 09:10:09 -0800

    updates

---
+++ create-login.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TITLE=$1
+LOGIN=$2
+PASS=$3
+
+./op get template Login                                                 \
+  | jq -r "$(cat << EOF | xargs
+        .fields
+          |= map( select(.name == \"username\").value = \"$LOGIN\"
+                | select(.name == \"password\").value = \"$PASS\"
+                )
+EOF
+)"                                                                      \
+  | ./op encode                                                         \
+  | xargs ./op create item Login --title="$TITLE" --vault=Personal
